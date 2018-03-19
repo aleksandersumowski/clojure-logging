69843e2bec791fc903adce98ea875f17d5923684
Aleksander Sumowski <aleksander.sumowski@youview.com>
2018-03-19 15:30:20 +0000

    bump

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.logging  "0.2.6"]
-                 [org.slf4j/jcl-over-slf4j   "1.7.7"]
-                 [ch.qos.logback/logback-classic "1.1.3"]
-                 [org.slf4j/slf4j-api "1.7.7"]
-                 [org.slf4j/jul-to-slf4j "1.7.7"]
-                 [org.slf4j/log4j-over-slf4j "1.7.7"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.logging  "0.4.0"]
+                 [org.slf4j/jcl-over-slf4j   "1.7.25"]
+                 [ch.qos.logback/logback-classic "1.2.3"]
+                 [org.slf4j/slf4j-api "1.7.25"]
+                 [org.slf4j/jul-to-slf4j "1.7.25"]
+                 [org.slf4j/log4j-over-slf4j "1.7.25"]]
   :main ^:skip-aot test-system-properties.core
   :target-path "target/%s"
   :jvm-opts ["-Droot-level-logging=DEBUG"]
