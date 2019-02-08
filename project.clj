9e251459b03d869510db608287b6ca732640b717
Aleksander Sumowski <aleksandersumowski@users.noreply.github.com>
2019-02-08 12:02:28 +0000

    bump

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.logging  "0.4.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/tools.logging  "0.4.1"]
                  [org.slf4j/jcl-over-slf4j   "1.7.25"]
                  [ch.qos.logback/logback-classic "1.2.3"]
                  [org.slf4j/slf4j-api "1.7.25"]
